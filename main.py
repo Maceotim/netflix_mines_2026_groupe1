58047db901e202015e1ade96a79a1e6f1034a9bc
dariusnuban <darius.nuban@etu.minesparis.psl.eu~>
2026-04-07 14:59:09 +0200

    ok

--- main.py
+++ main.py
@@ -164,4 +164,4 @@ def remove_preference(genre_id: int, user_id: int = Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
